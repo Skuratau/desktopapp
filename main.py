43d8e5fd72f06e772e4d1eb527ab6edacb6e30f8
Hleb Skuratau <hleb_skuratau@epam.com>
2018-08-22 08:29:45 +0300

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.lang import Builder
+
+
+class FMEEApp(App):
+    def build(self):
+        return pass
+
+if __name__ == '__main__';
+    FMEEApp().run()
